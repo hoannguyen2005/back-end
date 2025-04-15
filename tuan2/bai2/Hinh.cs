a0b8aa1d3a543e2bb932688e5995db4b723cbee1
hoannguyen2005 <161012480+hoannguyen2005@users.noreply.github.com>
2025-04-15 14:45:17 +0700

    Add files via upload

---
+++ tuan2/bai2/Hinh.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab15_bai2
+{
+   
+        public abstract class Hinh
+       {
+        public abstract double TinhChuVi();
+        public abstract double TinhDienTich();
+        public abstract string Ten();
+        }
+
+           
+        
+    
+}
